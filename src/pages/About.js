9a91396698c8ad6e4f1d25082a767286f643ea7d
Kallol Bairagi <kallolb22@gmail.com>
2020-05-03 07:52:49 +0530

    worked on about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
+import artist from '../images/img5.jpg'
+
 
 export default class About extends Component {
     render() {
         return (
-            <div>
-                hello from about us page
+            <div className="container-about">
+                <div className="card-about">
+                    <h2>About us</h2>
+                    <div className = "bar"></div>
+                    <div className="sub">Gig discovery like never before !</div>
+                    <p>We at Gigsutra, pride ourselves on our impeccable ability to connect artists and organizers. 
+                        This comes with our unique customer relations team who are at your beck and call 24/7/365.
+                        <br/><br/>For potential event organizers out there, we provide a one on one service to take care of all your event related needs. 
+                        From finding artists to complete event handling we give the decision making in your hands and just focus on providing quality service.
+                    </p>
+                </div>
+
+                <div className="card-artist">
+                    <h2>Artists</h2>
+                    <div className = "bar"></div>
+                    <div className="sub">Soon to launch our Artist Connect</div>
+                    <p>Using our Artist connect, Artists will be able to connect with peers and tet their content online. And the great news 
+                        is it's free.</p>
+                </div>
             </div>
         )
     }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,15 +2,24 @@ import React, { Component } from 'react'
 import Hero from '../components/Hero'
 import Banner from '../components/Banner'
 import {Link} from 'react-router-dom'
+import About from './About'
 
 export default class Home extends Component {
     render() {
         return (
-            <Hero>
-                <Banner title ="artist registrations" subtitle ="now open !">
-                    <Link to="/login" className="btn-prm" >register now</Link>
-                </Banner>
-            </Hero>
+            <div className="wrapper-about">
+                <Hero>
+                    <Banner title ="artist registrations" subtitle ="now open !">
+                        <Link to="/login" className="btn-prm" >register now</Link>
+                    </Banner>
+                </Hero>
+
+                <div >
+                    <About/>
+                </div>
+                
+            </div>
+        
         )
     }
 }
